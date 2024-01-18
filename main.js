f2fafff9619847c8bc2430a3cca8a8c884d70eca
Informatica <informatica@alumno.local>
2024-01-18 17:32:36 -0600

    ultima actualizacion

--- main.js
+++ main.js
@@ -4,33 +4,44 @@ const palabras = ['kinal',
                 'graduacion',
                 'sandia'];
 
-const numeroPalabra = Math.floor(Math.random = palabras.length);
-let letrasPalabra = Array(numeroPalabra.length).fill("_");
+const numeroPalabra = palabras[Math.floor(Math.random() * palabras.length)];        
+// Creador de espacios
+let letrasPalabra = Array(palabraAleatoria.length).fill("_");
 let intentos = 7;
 
-visualizacion_palabra = document.getElementById('palabraAdivinar');
-letraIngresada = document.getElementById('letraIngresada');
-personaje = document.getElementById('personajeAhorcado');
-intentosRestantes = document.getElementById('oportunidades');
-teclado = document.getElementById('teclado');
-entradaTecla = document.getElementById('entradaTecla');
+const visualizacionPalabra = document.getElementById("visualizacion-palabra");
+const intentosRestantesDisplay = document.getElementById("intentos-restantes");
+const entradaTecla = document.getElementById("entrada-tecla");
+const tecladoContenedor = document.getElementById("teclado");
+const modal = document.getElementById("miModal");
+const mensajeModal = document.getElementById("mensaje-modal");
+const recargar = document.getElementById("recargarPagina");
+const canvas = document.getElementById("ahorcado");
+const letrasUtilizadas = {};
+const ganador = document.getElementById("ganador");
+const perdedor = document. getElementById("perdedor");
+const textoGanador = document.getElementById("textoGanador");
+const textoPerdedor = document.getElementById("textoPerdedor");
+const palabraNoAdivinada = document.getElementById("palabraNoAdivinada");
 
-const alfabeto = [a-z];
+// Letras que apareceran en el teclado
+const alfabeto = "qwertyuiopasdfghjklñzxcvbnm";
 
-for(const letra of alfabeto){
-    const tecla = document.createElement('div');
+for (const letra of alfabeto) {
+    const tecla = document.createElement("div");
     tecla.classList.add("tecla");
     tecla.textContent = letra;
     tecla.addEventListener("click", function() {
         entradaTecla.value = letra;
         manejarTecla();
     });
-    teclacoContenedor.appendChild(tecla);
+    tecladoContenedor.appendChild(tecla);
 }
-function actualizarVisualizacion(){
-    visualizacion_palabra.textContent = letrasPalabra.join(" ");
-    intentosRestantes.textContent = intentos;
-}
+        
+function actualizarVisualizacion() {
+    visualizacionPalabra.textContent = palabraAdivinada.join(" ");
+    intentosRestantesDisplay.textContent = intentosRestantes;
+}   
 function manejarTecla(){
     const tecla = entradaTecla.value.toLowerCase();
     entradaTecla.value = "";
