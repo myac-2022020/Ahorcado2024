3a226bb0f8a13aa43bf2585e3fbd41d4b9e07c1d
Informatica <informatica@alumno.local>
2024-01-18 16:16:58 -0600

    Primera gestion

---
+++ main.js
@@ -0,0 +1,10 @@
+var palabras = ['kinal',
+                'hola',
+                'ultimo',
+                'graduacion'];
+
+var numeroPalabra = Math.lengt(Math.random = palabras.length);
+
+palabraMisterio = document.getElementById('palabraAdivinar');
+letraIngresada = document.getElementById('letraIngresada');
+personaje = document.getElementById('personajeAhorcado');
